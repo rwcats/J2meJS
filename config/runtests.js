acf6874914348b1653b518e5a083e3252d0f8440
rwcats <30314539+rwcats@users.noreply.github.com>
2025-11-02 02:29:25 +0700

    Update runtests.js

--- config/runtests.js
+++ config/runtests.js
@@ -2,6 +2,14 @@ config.jars = "jar/Anyview4.0.jar";
 config.jad = "jar/Anyview4.0.jad";
 config.midletClassName = "com.ismyway.anyview.Anyview";
 
+// Fix đường dẫn để chạy đúng cả Cloudflare và GitHub
+if (!config.jars.startsWith("http")) {
+  config.jars = "./" + config.jars.replace(/^\/+/, "");
+}
+if (config.jad && !config.jad.startsWith("http")) {
+  config.jad = "./" + config.jad.replace(/^\/+/, "");
+}
+
 MIDlet.shouldStartBackgroundService = function() {
   return fs.exists("/startBackgroundService");
 };
